0e7d64aebdc89f847595f448e5372ec577fd06f1
YashPanchal1508 <110219476+YashPanchal1508@users.noreply.github.com>
2024-03-06 15:26:13 +0530

    update

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,12 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    connectionString: process.env.POSTGRES_URL,
+    // connectionString: process.env.POSTGRES_URL,
+    host: process.env.POSTGRES_HOST,
+    database: process.env.POSTGRES_DB,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    port: process.env.PORT,
   })
 
 pool.connect((err) => {
